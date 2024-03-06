05a87ba50cc986a98d4401ea56ef30d43c991082
Alanis3a1b <alanis.alvarez@catolica.edu.sv>
2024-03-06 15:56:54 -0600

    Corrigiendo para practicar - cree una clase restaurante.cs para almacenar en solo una los parámetros de cada tabla.

---
+++ L01_2021-AB-601/Models/restaurante.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01_2021_AB_601.Models
+{
+    public class restaurante
+    {
+    }
+
+    public class clientes
+    {
+        [Key]
+        public int clienteId { get; set; }
+        public string nombreCliente { get; set; }
+        public string direccion { get; set; }
+
+    }
+
+    public class motoristas
+    {
+        [Key]
+        public int motoristaId { get; set; }
+        public string nombreMotorista { get; set; }
+    }
+
+    public class pedidos
+    {
+        [Key]
+        public int pedidoId { get; set; }
+        public int motoristaId { get; set; }
+        public int clienteId { get; set; }
+        public int platoId { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+
+    }
+
+    public class platos
+    {
+        [Key]
+        public int platoId { get; set; }
+        public string nombrePlato { get; set; }
+        public decimal precio { get; set; }
+    }
+
+}
